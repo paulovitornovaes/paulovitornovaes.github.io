a9dd6f396189bf6e7e18ab3469721dd970486f6a
Paulo Vitor <72573443+paulovitornovaes@users.noreply.github.com>
2021-09-10 09:02:20 -0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var hamburguer = document.querySelector(".hamburguer");
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("show-menu")
 
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".sidebar").classList.toggle("show-menu");
-});
+);
